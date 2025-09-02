ff4605bf83a37856598cbadf640acf1ea3c73754
Jona <llamasjonat@gmail.com>
2025-09-01 23:26:08 -0300

    poc(lib): integrate timeAgo on Utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,37 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function timeAgo(timestamp: number): string {
+  const now = Date.now();
+  const secondsAgo = Math.floor((now - timestamp * 1000) / 1000);
+
+  const minutes = Math.floor(secondsAgo / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+
+  if (secondsAgo < 60) {
+    return 'now';
+  } else if (minutes < 60) {
+    return `${minutes}m`;
+  } else if (hours < 24) {
+    return `${hours}h`;
+  } else if (days < 7) {
+    return `${days}d`;
+  } else {
+    const date = new Date(timestamp * 1000);
+    const day = date.getDate();
+    const month = date.toLocaleString('en-US', { month: 'short' });
+    const year = date.getFullYear();
+    const currentYear = new Date().getFullYear();
+
+    if (year === currentYear) {
+      return `${day} ${month}`;
+    } else {
+      return `${day} ${month} ${year}`;
+    }
+  }
 }
